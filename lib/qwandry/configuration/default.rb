fcd76d89ee623b12a46e9c55603477bd9cbe785c
Adam Sanderson <netghost@gmail.com>
2010-12-30 12:52:16 -0700

    Adding support for node.js

--- lib/qwandry/configuration/default.rb
+++ lib/qwandry/configuration/default.rb
@@ -41,6 +41,17 @@ register :python do
   add python_paths, :class=>Qwandry::LibraryRepository, :reject => /\.(py[oc])|(egg-info)$/
 end
 
+# Add node.js repositories:
+register :node do
+  # Execute a node script to find all of the load paths:
+  node_script_path = File.join(File.dirname(__FILE__),'probe_node.js')
+  node_paths = `node #{node_script_path}` rescue ''
+  
+  node_paths = node_paths.split("\n")
+  # Add the python paths, instruct Qwandry to skip any compiled files when trying to match a file/library:
+  add node_paths
+end
+
 # Qwandry is a ruby app after all, so activate ruby and gem by default.  Other defaults can be set
 # with a custom init.rb
 default :ruby, :gem
