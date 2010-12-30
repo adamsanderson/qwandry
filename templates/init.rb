5ab6901ab1126ec2f8563177ce2541cb2a720494
Adam Sanderson <netghost@gmail.com>
2010-12-30 11:03:02 -0700

    Updated custom init.rb to reflect how Qwandry expects to get configured.\nHopefully this does not break for anyone.  I am assuming nobody has been too adventurous with it yet.

--- templates/init.rb
+++ templates/init.rb
@@ -4,47 +4,31 @@
 # 
 # To learn more about Qwandry, take a look at the source with qwandry!
 #     qw qwandry
-
+#
 # You can uncomment the indented code to try it out.
 # If you get confused, just delete this file and run the customize command again.
-
+#
 # == Adding Personal Projects
 # 
 # If you keep all your projects in the home directory under projects,
 # you can tell Qwandry to search there.
 # 
-#   add :projects, "~/Projects/"
+#     register 'projects' do
+#       add '~/Projects/personal'
+#       add '~/Projects/work'
+#       add '~/Experiments'
+#     end
+# 
+# This will tell Qwandry that you want to create a `projects` configuration
+# 
+# For more advanced examples, try `qw qwandry` and take a look at 
+# `configuration/default.rb`.
 
 # == Setting Defaults
 # To make `projects` a default search path, activate `projects`:
 # 
-#   activate :projects
+#   default :projects
 #
-# If you don't use ruby, you can make something else the default:
+# You can also set multiple configurations to be the default:
 # 
-#   deactivate :ruby, :gem
-#   activate :python
-
-# == Common Repositories
-# Below are some common configurations you might find useful, these may vary
-# from system to system.
-
-# = Haskel
-# Open Cabal packages:
-#   add :ghc, "~/.cabal/lib"
-# Open Haskel system libraries:
-#   add :ghc, "/usr/local/lib/ghc/"
-
-# = Ruby
-# Qwandry comes set up for ruby by default, however you may want to be able to
-# quickly access the ruby sources.
-# 
-# If you installed ruby with `rvm`, your path might be something like this:
-#   add :cruby, "~/.rvm/src/ruby-1.9.1-p378/"
-
-# = Javascript / Node
-# Edit node.js and npm managed libraries.
-# Configure a :node repository showing only the active versions of each library:
-#  add :node, "/usr/local/lib/node/", :reject=> /@/ 
-# # Configure a :npm repository showing only the versioned libraries:
-#  add :npm, "/usr/local/lib/node/",  :accept=> /@/
+#   default :python, :projects
